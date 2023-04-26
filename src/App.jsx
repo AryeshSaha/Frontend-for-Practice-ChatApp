030b9b452ca013c38b6104f9f59c117de0a499ca
AryeshSaha <aryeshsaha5302@gmail.com>
2023-04-26 12:31:55 +0530

    service worker registration failed testing 1

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   // service worker registration
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('../service-worker.js')
         .then(registration => {
           console.log('Service worker registered:', registration);
         })
