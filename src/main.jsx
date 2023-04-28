e736d3832070742ffc0e2ef4641548e0cfcb23cb
AryeshSaha <aryeshsaha5302@gmail.com>
2023-04-29 00:28:57 +0530

    service worker fail testing 12? maybe?

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import ChatProvider from "./Context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
 
-// service worker registration
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("../sw.js")
-      .then((registration) => {
-        console.log("ServiceWorker registered: ", registration);
-      })
-      .catch((error) => {
-        console.log("ServiceWorker registration failed: ", error);
-      });
-  });
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Aryesh's Chat-A-Lot",
+        short_name: "Chat-A-Lot",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "./assets/maskable_icon.png",
+            sizes: "196x196",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "./assets/logo192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./assets/logo256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "./assets/logo384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "./assets/logo512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+    }),
+  ],
   optimizeDeps: {
     // include: ['linked-dep'],
   },
@@ -12,7 +58,7 @@ export default defineConfig({
       transformMixedEsModules: true,
       // include: [/linked-dep/, /node_modules/],
     },
-    base: '/',
+    base: "/",
     ssr: false,
   },
-})
+});
